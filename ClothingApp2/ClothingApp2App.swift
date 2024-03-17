11ecf0d924d1c5ca4f9008472e9a9293dcf0687e
chamod101 <chamoddperera99@gmail.com>
2024-03-17 07:32:18 -0700

    Initial Commit

---
+++ ClothingApp2/ClothingApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingApp2App.swift
+//  ClothingApp2
+//
+//  Created by Chamod Dilushanka on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct ClothingApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
